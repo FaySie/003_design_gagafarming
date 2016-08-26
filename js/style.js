6e8f6f173d683ed5311708a715b116b25b1b11f2
FaySie <sel12342001@gmail.com>
2016-08-26 17:05:06 +0800

    add button hover & mouseWheel animate

--- js/style.js
+++ js/style.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-	
+	var page = $('.page');
+	page.find('li').on('click', function(){
+		page.find('li').removeClass('active');
+		$(this).addClass('active');	
+	});
 });
+
